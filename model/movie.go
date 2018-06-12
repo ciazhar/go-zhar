19ca13e1d6e7f7cb59399514c8dd96c5f35e3659
ciazhar <ciazhar.id@gmail.com>
2018-06-12 22:55:44 +0700

    create model

---
+++ model/movie.go
@@ -0,0 +1,10 @@
+package model
+
+import "gopkg.in/mgo.v2/bson"
+
+type Movie struct {
+	ID          bson.ObjectId `bson:"_id" json:"id"`
+	Name        string        `bson:"name" json:"name"`
+	CoverImage  string        `bson:"cover_image" json:"cover_image"`
+	Description string        `bson:"description" json:"description"`
+}
