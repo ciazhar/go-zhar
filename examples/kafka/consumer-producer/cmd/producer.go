532d3b3d8d6025b6218d70f99f44ad6f99543c84
ciazhar <ciazhar.id@gmail.com>
2024-04-15 13:32:49 +0700

    kafka consumer and producer example

---
+++ examples/kafka/consumer-producer/cmd/producer.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/ciazhar/go-zhar/examples/kafka/consumer-producer/model"
+	"github.com/ciazhar/go-zhar/pkg/env"
+	"github.com/ciazhar/go-zhar/pkg/kafka"
+	"github.com/ciazhar/go-zhar/pkg/logger"
+	"github.com/spf13/viper"
+)
+
+func main() {
+
+	// Logger
+	log := logger.Init()
+
+	// Environment configuration
+	env.Init("config.json", log)
+
+	producer := kafka.NewSyncProducer(viper.GetString("kafka.brokers"), log)
+	defer producer.Close()
+
+	producer.PublishMessage(model.TopicName, "Hello, Kafka!")
+	producer.PublishMessage(model.Topic2Name, "Hello, Kafka!")
+}
---
+++ examples/kafka/consumer-producer/model/kafka_topic.go
@@ -0,0 +1,6 @@
+package model
+
+const (
+	TopicName  = "test_topic"
+	Topic2Name = "test_topic_2"
+)
