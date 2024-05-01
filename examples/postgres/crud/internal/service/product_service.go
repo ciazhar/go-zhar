f0a79a129f04f2f5f5bff2698ad1052637d7bcdb
ciazhar <ciazhar.id@gmail.com>
2024-05-01 22:43:03 +0700

    improve cursor searching and implement encrypt decrypt cursor

--- examples/postgres/crud/internal/service/product_service.go
+++ examples/postgres/crud/internal/service/product_service.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	db "github.com/ciazhar/go-zhar/examples/postgres/crud/internal/generated/repository"
+	"github.com/ciazhar/go-zhar/examples/postgres/crud/internal/model"
 	"github.com/ciazhar/go-zhar/pkg/db_util"
 	"github.com/ciazhar/go-zhar/pkg/logger"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -26,7 +28,6 @@ type productService struct {
 }
 
 func (p productService) GetProductsCursor(ctx context.Context, name string, price float64, cursor string, size int) (db_util.PageCursor, error) {
-
 	countProducts, err := p.queries.CountProducts(ctx, db.CountProductsParams{
 		Name:  name,
 		Price: price,
@@ -35,141 +36,98 @@ func (p productService) GetProductsCursor(ctx context.Context, name string, pric
 		return db_util.PageCursor{}, err
 	}
 
-	nextPrev, id, currPage, err := db_util.ParseCursor(cursor)
+	decodeString, err := base64.StdEncoding.DecodeString(cursor)
 	if err != nil {
 		return db_util.PageCursor{}, err
 	}
 
-	if nextPrev == "next" {
+	nextPrev, id, currPage, err := db_util.ParseCursor(string(decodeString))
+	if err != nil {
+		return db_util.PageCursor{}, err
+	}
+
+	var products []model.Product
+	switch nextPrev {
+	case "next":
+		fallthrough
+	case "prev":
 		idI, err := strconv.Atoi(id)
 		if err != nil {
 			return db_util.PageCursor{}, err
 		}
-		products, err := p.queries.GetProductsNextCursor(ctx, db.GetProductsNextCursorParams{
-			Name:   fmt.Sprintf("%%%s%%", name),
-			Price:  price,
-			Cursor: int32(idI),
-			Si:     int32(size),
-		})
-		if err != nil {
-			return db_util.PageCursor{}, err
-		}
+		if nextPrev == "next" {
+			p, err := p.queries.GetProductsNextCursor(ctx, db.GetProductsNextCursorParams{
+				Name:   fmt.Sprintf("%%%s%%", name),
+				Price:  price,
+				Cursor: int32(idI),
+				Si:     int32(size),
+			})
+			if err != nil {
+				return db_util.PageCursor{}, err
+			}
 
-		if len(products) == 0 {
-			return db_util.PageCursor{}, errors.New("data not found")
-		}
+			products = ConvertGetProductsNextCursorRowArrayToProductArray(p)
 
-		totalPage := db_util.CountPageSize(int(countProducts), size)
-
-		if currPage > totalPage {
-			return db_util.PageCursor{}, errors.New("page not found")
-		}
-
-		nextCursor := ""
-		if totalPage == currPage {
-			nextCursor = ""
 		} else {
-			nextCursor = fmt.Sprintf("next,%d,%d", products[len(products)-1].ID, currPage+1)
-		}
+			p, err := p.queries.GetProductsPrevCursor(ctx, db.GetProductsPrevCursorParams{
+				Cursor: int32(idI),
+				Name:   fmt.Sprintf("%%%s%%", name),
+				Price:  price,
+				Si:     int32(size),
+			})
+			if err != nil {
+				return db_util.PageCursor{}, err
+			}
 
-		prevCursor := ""
-		if currPage == 1 {
-			prevCursor = ""
-		} else {
-			prevCursor = fmt.Sprintf("prev,%d,%d", products[0].ID, currPage-1)
+			products = ConvertGetProductsPrevCursorRowArrayToProductArray(p)
 		}
-
-		return db_util.PageCursor{
-			Data:        products,
-			TotalData:   int(countProducts),
-			CurrentPage: currPage,
-			TotalPage:   totalPage,
-			NextCursor:  nextCursor,
-			PrevCursor:  prevCursor,
-		}, nil
-	} else if nextPrev == "prev" {
-		idI, err := strconv.Atoi(id)
-		if err != nil {
-			return db_util.PageCursor{}, err
-		}
-		products, err := p.queries.GetProductsPrevCursor(ctx, db.GetProductsPrevCursorParams{
-			Cursor: int32(idI),
-			Name:   fmt.Sprintf("%%%s%%", name),
-			Price:  price,
-			Si:     int32(size),
-		})
-		if err != nil {
-			return db_util.PageCursor{}, err
-		}
-
-		if len(products) == 0 {
-			return db_util.PageCursor{}, errors.New("data not found")
-		}
-
-		totalPage := db_util.CountPageSize(int(countProducts), size)
-
-		if currPage > totalPage {
-			return db_util.PageCursor{}, errors.New("page not found")
-		}
-
-		nextCursor := ""
-		if totalPage == currPage {
-			nextCursor = ""
-		} else {
-			nextCursor = fmt.Sprintf("next,%d,%d", products[len(products)-1].ID, currPage+1)
-		}
-
-		prevCursor := ""
-		if currPage == 1 {
-			prevCursor = ""
-		} else {
-			prevCursor = fmt.Sprintf("prev,%d,%d", products[0].ID, currPage-1)
-		}
-
-		return db_util.PageCursor{
-			Data:        products,
-			TotalData:   int(countProducts),
-			CurrentPage: currPage,
-			TotalPage:   totalPage,
-			NextCursor:  nextCursor,
-			PrevCursor:  prevCursor,
-		}, nil
-	} else {
-		products, err := p.queries.GetProductsCursor(ctx, db.GetProductsCursorParams{
+	case "":
+		fallthrough
+	default:
+		p, err := p.queries.GetProductsCursor(ctx, db.GetProductsCursorParams{
 			Name:  fmt.Sprintf("%%%s%%", name),
 			Price: price,
 			Si:    int32(size),
 		})
+
 		if err != nil {
 			return db_util.PageCursor{}, err
 		}
 
-		if len(products) == 0 {
-			return db_util.PageCursor{}, errors.New("data not found")
-		}
-
-		totalPage := db_util.CountPageSize(int(countProducts), size)
-
-		if currPage > totalPage {
-			return db_util.PageCursor{}, errors.New("page not found")
-		}
-
-		nextCursor := ""
-		if totalPage == currPage {
-			nextCursor = ""
-		} else {
-			nextCursor = fmt.Sprintf("next,%d,%d", products[len(products)-1].ID, currPage+1)
-		}
-
-		return db_util.PageCursor{
-			Data:        products,
-			TotalData:   int(countProducts),
-			CurrentPage: 1,
-			TotalPage:   totalPage,
-			NextCursor:  nextCursor,
-			PrevCursor:  "",
-		}, nil
+		products = ConvertGetProductsCursorRowArrayToProductArray(p)
 	}
+
+	if len(products) == 0 {
+		return db_util.PageCursor{}, errors.New("data not found")
+	}
+
+	totalPage := db_util.CountPageSize(int(countProducts), size)
+
+	if currPage > totalPage {
+		return db_util.PageCursor{}, errors.New("page not found")
+	}
+
+	nextCursor := ""
+	if totalPage > currPage {
+		nextCursor = fmt.Sprintf("next,%d,%d", products[len(products)-1].ID, currPage+1)
+	}
+
+	prevCursor := ""
+	if currPage > 1 {
+		prevCursor = fmt.Sprintf("prev,%d,%d", products[0].ID, currPage-1)
+	}
+
+	nextCursor = base64.StdEncoding.EncodeToString([]byte(nextCursor))
+	prevCursor = base64.StdEncoding.EncodeToString([]byte(prevCursor))
+
+	return db_util.PageCursor{
+		Data:        products,
+		TotalData:   int(countProducts),
+		CurrentPage: currPage,
+		TotalPage:   totalPage,
+		NextCursor:  nextCursor,
+		PrevCursor:  prevCursor,
+	}, nil
 }
 
 func (p productService) UpdateProductPrice(ctx context.Context, id int, name string, price float64) error {
@@ -223,6 +181,46 @@ func (p productService) GetProducts(ctx context.Context, name string, price floa
 		TotalPage: db_util.CountPageSize(int(countProducts), size),
 	}, nil
 }
+
+func ConvertGetProductsNextCursorRowArrayToProductArray(rows []db.GetProductsNextCursorRow) []model.Product {
+	var result []model.Product
+	for _, row := range rows {
+		result = append(result, model.Product{
+			ID:        row.ID,
+			Name:      row.Name,
+			Price:     row.Price,
+			CreatedAt: row.CreatedAt,
+		})
+	}
+	return result
+}
+
+func ConvertGetProductsPrevCursorRowArrayToProductArray(rows []db.GetProductsPrevCursorRow) []model.Product {
+	var result []model.Product
+	for _, row := range rows {
+		result = append(result, model.Product{
+			ID:        row.ID,
+			Name:      row.Name,
+			Price:     row.Price,
+			CreatedAt: row.CreatedAt,
+		})
+	}
+	return result
+}
+
+func ConvertGetProductsCursorRowArrayToProductArray(rows []db.GetProductsCursorRow) []model.Product {
+	var result []model.Product
+	for _, row := range rows {
+		result = append(result, model.Product{
+			ID:        row.ID,
+			Name:      row.Name,
+			Price:     row.Price,
+			CreatedAt: row.CreatedAt,
+		})
+	}
+	return result
+}
+
 func NewProductService(
 	queries *db.Queries,
 	db *pgxpool.Pool,
