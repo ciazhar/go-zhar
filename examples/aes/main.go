fa3c9ee3be321ee153376ce4a1ed4a089279fe63
ciazhar <ciazhar.id@gmail.com>
2024-05-06 23:34:13 +0700

    aes example

---
+++ examples/aes/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ciazhar/go-zhar/pkg/aes"
+)
+
+func main() {
+	key := aes.GenerateKey()
+	fmt.Println("Key:", key)
+
+	plaintext := "Hello, AES in Go!"
+
+	// Encrypt
+	ciphertext := aes.Encrypt(plaintext, key)
+	fmt.Println("Encrypted:", ciphertext)
+
+	// Decrypt
+	decryptedText := aes.Decrypt(ciphertext, key)
+	fmt.Println("Decrypted:", decryptedText)
+}
