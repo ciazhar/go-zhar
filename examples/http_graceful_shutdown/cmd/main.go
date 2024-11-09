c305387c2891386e945b8535fa23de7969ef3d55
ciazhar <ciazhar.id@gmail.com>
2024-11-09 21:56:17 +0700

    using logger

--- examples/http_graceful_shutdown/cmd/main.go
+++ examples/http_graceful_shutdown/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"github.com/ciazhar/go-start-small/pkg/logger"
 	"os/signal"
 	"sync"
 	"syscall"
@@ -28,9 +28,9 @@ func main() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		fmt.Println("Starting Fiber server on :8080...")
+		logger.LogInfo(ctx, "Starting Fiber server on :8080...", nil)
 		if err := app.Listen(":8080"); err != nil {
-			fmt.Printf("Fiber server failed: %v\n", err)
+			logger.LogFatal(ctx, err, "Failed to start Fiber server", nil)
 		}
 	}()
 
@@ -43,7 +43,7 @@ func main() {
 
 	// Listen for shutdown signal
 	<-ctx.Done() // Block until signal is received
-	fmt.Println("\nShutting down gracefully...")
+	logger.LogInfo(ctx, "Received shutdown signal...", nil)
 
 	// Start graceful shutdown process with a 10-second timeout
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -51,25 +51,25 @@ func main() {
 
 	// Gracefully shutdown Fiber app
 	if err := app.ShutdownWithContext(shutdownCtx); err != nil {
-		fmt.Printf("Error shutting down Fiber server: %v\n", err)
+		logger.LogFatal(ctx, err, "Error shutting down Fiber server", nil)
 	} else {
-		fmt.Println("Fiber server shut down successfully.")
+		logger.LogInfo(ctx, "Fiber server shut down gracefully.", nil)
 	}
 
 	// Wait for background tasks to complete
 	wg.Wait()
-	fmt.Println("All goroutines completed.")
+	logger.LogInfo(ctx, "All goroutines completed.", nil)
 }
 
 func backgroundWorker(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done(): // Listen for shutdown signal
-			fmt.Println("Background worker shutting down...")
+			logger.LogInfo(ctx, "Background worker shutting down...", nil)
 			return
 		default:
 			// Simulate work
-			fmt.Println("Background worker is working...")
+			logger.LogInfo(ctx, "Background worker is working...", nil)
 			time.Sleep(2 * time.Second)
 		}
 	}
