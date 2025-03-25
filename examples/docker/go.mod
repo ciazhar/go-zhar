12b003efd70f0893a46c64c6416bd8e7f94f3f9b
ciazhar <ciazhar.id@gmail.com>
2025-03-25 16:09:28 +0700

    docker example

---
+++ examples/docker/go.mod
@@ -0,0 +1,3 @@
+module docker
+
+go 1.24.1
---
+++ examples/docker/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Docker! 🚀")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server berjalan di port 8080")
+	http.ListenAndServe(":8080", nil)
+}
